70fa5efd89b3b28d732653c99fc9ccf8818feb5c
Maxim Kuznecov <maksimkuznetsov@topverstka.ru>
2022-03-12 14:28:18 +0300

    1

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ import cors  from 'cors'
 const PORT = 5050
 const app = express()
 
+// делаем доступным JSON
 app.use(express.json())
+// отключаем cors политики
 app.use(cors())
-
+// дефолтный эндпоинт
 app.get('/api/', (req, res) => {
-
     res.json({name: 'Max', age: 34})
 })
 
 app.listen(PORT, () => {
-
-
     console.log('server started!')
 })
+
